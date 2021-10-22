a6f3c82a3d8da74132b6df2379899541ead503a6
Evandro Damaso <68165752+dam450@users.noreply.github.com>
2021-10-21 23:48:41 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+const socialMediaData = {
+  github: 'dam450',
+  youtube: 'user/D3v14nz',
+  facebook: '',
+  instagram: 'ev4nd7o',
+  twitter: 'ev4nd7o'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://www.${social}.com/${socialMediaData[social]}`
+  }
+}
+
+function getGithubUserData(user) {
+  const url = `https://api.github.com/users/${user}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      gitUserName.textContent = data.login
+      gitBio.textContent = data.bio
+      gitLink.href = data.html_url //`http://www.github.com/${data.login}`
+      photo.src = data.avatar_url
+    })
+}
+
+changeSocialMediaLinks()
+getGithubUserData(socialMediaData.github)
